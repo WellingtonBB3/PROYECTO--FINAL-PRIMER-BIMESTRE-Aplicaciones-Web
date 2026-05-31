8c1bfae60a2df0bb53e1681ff668cd5949c6e97b
Barros Benavides Wellington Jair <wellington.barros@epn.edu.ec>
2026-05-31 12:45:25 -0500

    Add files via upload

---
+++ objects/Thief.js
@@ -0,0 +1,44 @@
+import Phaser from 'phaser';
+
+export default class Thief extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, options = {}) {
+    const {
+      texture = 'choro-0',
+      displayWidth = 250,
+      displayHeight = 312,
+      bodyWidth = 80,
+      bodyHeight = 160,
+    } = options;
+
+    super(scene, x, y, texture);
+
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+
+    this.setDisplaySize(displayWidth, displayHeight);
+
+    this.body.setSize(bodyWidth, bodyHeight, true);
+    this.body.setSize(bodyWidth, bodyHeight);
+
+    this.body.setOffset(
+      (this.width - bodyWidth) / 2,
+      this.height - bodyHeight
+    );
+
+    this.setCollideWorldBounds(true);
+
+    this.anims.play('choro-run');
+  }
+
+  follow(target, speed = 200) {
+    if (!this.active || !target?.active) return;
+
+    if (this.x > target.x) {
+      this.setVelocityX(-speed);
+      this.flipX = true;
+    } else {
+      this.setVelocityX(speed);
+      this.flipX = false;
+    }
+  }
+}
